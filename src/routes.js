10769b6a70a055678899c3be35660d16441f6c95
Marcos Aurélio <marcosaureliolelo@outlook.com>
2019-08-25 01:49:52 -0300

    Real-time settings

--- src/routes.js
+++ src/routes.js
@@ -10,10 +10,8 @@ const upload = multer(uploadConfig);
 router.post('/chat', upload.single('image'), userController.index);
 
 router.post("/message/:nickname", async (req, res) => {
-    const sendBack = `message '${req.body.msg}' as been sended by '${req.params.nickname}'`
-    await messages.push({ portrait: req.body.portrait, nickname: req.params.nickname, msg: req.body.msg, date: req.body.date })
-    res.status(201).send(sendBack)
+    await messages.push({ nickname: req.params.nickname, msg: req.body.msg })
     req.io.emit('newMessage', messages);
-})
+});
 
 module.exports = router;
