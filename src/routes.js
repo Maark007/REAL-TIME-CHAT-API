d25d71f102fd6ebf50e1338467cd194b5de52b7b
Marcos Aurélio <marcosaureliolelo@outlook.com>
2019-08-27 05:43:44 -0300

    Real-time settings

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,22 @@
-const router = require('express').Router()
-
-const messages = []
-
+const router = require('express').Router();
+ 
+const bodyParser = require('body-parser');
+ 
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+ 
+let messages = [];
+ 
 router.post("/message", async (req, res) => {
     await messages.push({ nickname: req.body.nickname, msg: req.body.msg, img: req.body.img })
-    req.io.emit('newMessage', messages);
+ 
+    try {
+        req.io.emit('newMessage', messages);
+ 
+        return res.status(201).send("CREATED.")
+    } catch (e) {
+        return res.status(500).send("ERROR => " + e)
+    }
 })
 
-module.exports = router;
+module.exports = router
